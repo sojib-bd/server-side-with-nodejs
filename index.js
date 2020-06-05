fdbcef707c8b2595d04a2268c664481a8e7230e5
sojib-bd <sojibrahmatuzzaman@gmail.com>
2020-06-05 23:41:12 +0600

    fixing the submit button with fetching

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ const Datastore = require('nedb');
 app.use(express.static('public'))
 app.use(express.json())
 
-const database = new Datastore('database.db');
+const database = new Datastore('datastore.db');
 
 database.loadDatabase();
-database.insert({ name: "sojib" })
+
+
+
 app.post('/api', (req, res) => {
     console.log('I got request from the client side')
     const data = req.body;
-    const timestamp = Date.now();
+    let timestamp = Date.now();
     data.timestamp = timestamp;
+    console.log(timestamp)
     database.insert(data)
 
     res.json({
