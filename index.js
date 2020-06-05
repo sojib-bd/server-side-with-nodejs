04d67d528857ab94db4ad5627b452d45730bfb9d
sojib-bd <sojibrahmatuzzaman@gmail.com>
2020-06-05 21:47:49 +0600

    making a server

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => console.log("listening from port 3000"))
 
 app.use(express.static('public'))
+app.use(express.json())
+
+const clientData = [];
+
+app.post('/api', (req, res) => {
+    console.log('I got request from the client side')
+    console.log(req.body);
+    const data = req.body;
+    clientData.push(data)
+    res.json({
+        status: 'success',
+        latitude: data.lat,
+        longitude: data.lon
+    })
+
+
+    console.log(clientData)
+
+})
+
+app.listen(3000, () => console.log("listening from port 3000"))  
