7ad66be27d18aba0fdb124a4e85f7ab0a7006f0d
sojib-bd <sojibrahmatuzzaman@gmail.com>
2020-06-05 22:52:13 +0600

    adding the Nedb database

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 const express = require('express');
 const app = express();
+const Datastore = require('nedb');
 
 
 app.use(express.static('public'))
 app.use(express.json())
 
-const clientData = [];
+const database = new Datastore('database.db');
 
+database.loadDatabase();
+database.insert({ name: "sojib" })
 app.post('/api', (req, res) => {
     console.log('I got request from the client side')
-    console.log(req.body);
     const data = req.body;
-    clientData.push(data)
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data)
+
     res.json({
         status: 'success',
+        timestamp: timestamp,
         latitude: data.lat,
         longitude: data.lon
     })
 
 
-    console.log(clientData)
+
 
 })
 
